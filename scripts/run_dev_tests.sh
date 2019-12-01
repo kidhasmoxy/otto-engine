f47812937c58116c95e2cc750b9ecdc76941a175
sheaffej <sheaffej@gmail.com>
2019-12-01 08:16:43 -0800

    update

--- scripts/run_dev_tests.sh
+++ scripts/run_dev_tests.sh
@@ -4,7 +4,7 @@ CONTAINER_NAME=otto-engine
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
-docker run --rm \
+docker run --rm -it \
 -e PYTEST_ADDOPTS="--color=yes" \
 -v $DIR/..:/app \
 $CONTAINER_NAME bash -c "/app/tests/run_tests.sh $1; ls -la /app; ls -la /config"
